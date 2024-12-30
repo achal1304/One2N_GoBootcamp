43482f854b47c9385b2b541fb5f28c5e78a06daf
achal1304 <achal20000@gmail.com>
2024-12-30 16:55:03 +0530

    Story8 | Performance | splitting lines and reading. Succesfuly  reading 3GB File

--- wordcount/wchandler/processer.go
+++ wordcount/wchandler/processer.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"os"
 	"sync"
-	"unicode"
 
 	"github.com/achal1304/One2N_GoBootcamp/wordcount/contract"
 	"github.com/achal1304/One2N_GoBootcamp/wordcount/wcerrors"
@@ -44,7 +43,7 @@ func ProcessWCCommand(wg *sync.WaitGroup,
 		reader = file
 	}
 
-	readUsingBuffers(fileName, wcValuesCh, reader)
+	ReadUsingBuffers(fileName, wcValuesCh, reader)
 }
 
 func ComputeTotalCount(multipleFiles bool,
@@ -74,14 +73,29 @@ func ComputeTotalCount(multipleFiles bool,
 	return 0
 }
 
-func readUsingBuffers(fileName string,
+func ReadUsingBuffers(fileName string,
 	wcValuesCh chan contract.WcValues,
 	reader io.Reader) {
-	buffer := make([]byte, 4096)
+	buffer := make([]byte, 1024*1024)
 	// lineBuffer := bytes.Buffer{}
 
 	wcCounterValues := contract.WcValues{FileName: fileName}
-	leftOver := ""
+	resultCh := make(chan contract.WcValues)
+	done := make(chan struct{})
+	var wg sync.WaitGroup
+	leftOver := []byte{}
+
+	go func(resultCh chan contract.WcValues, done chan struct{},
+		wcCounterValues *contract.WcValues) {
+		for res := range resultCh {
+			wcCounterValues.LineCount += res.LineCount
+			wcCounterValues.CharacterCount += res.CharacterCount
+			wcCounterValues.WordCount += res.WordCount
+		}
+		close(done)
+	}(resultCh, done, &wcCounterValues)
+
+	workerPool := make(chan struct{}, 10)
 	for {
 		n, err := reader.Read(buffer)
 		if err != nil && !errors.Is(err, io.EOF) {
@@ -90,40 +104,70 @@ func readUsingBuffers(fileName string,
 			return
 		}
 
-		text := leftOver + string(buffer[:n])
-		leftOver = ""
+		text := append(leftOver, buffer[:n]...)
+		leftOver = nil
 		lines := bytes.Split([]byte(text), []byte("\n"))
-		for i, line := range lines {
-			if i == len(lines)-1 {
-				leftOver = string(line)
-			} else {
+		totalLines := len(lines)
+		leftOver = lines[totalLines-1]
+
+		lines = lines[:totalLines-1]
+
+		wg.Add(1)
+		workerPool <- struct{}{} // Acquire a worker
+		go func(wg *sync.WaitGroup, lines [][]byte, resultCh chan contract.WcValues) {
+			defer wg.Done()
+			var wcCounterValues contract.WcValues
+			for _, line := range lines {
 				// adding \n character as we are splitting based on that which excludes the character
 				line = append(line, '\n')
 				wcCounterValues.CharacterCount += len(line)
 				wcCounterValues.LineCount++
-				wcCounterValues.WordCount += countWords(string(line))
+				wcCounterValues.WordCount += len(bytes.Fields(line))
 			}
 
-		}
+			resultCh <- wcCounterValues
+			<-workerPool // Release the worker
+		}(&wg, append([][]byte(nil), lines...), resultCh)
 
 		if errors.Is(err, io.EOF) {
 			break
 		}
 	}
+
+	wg.Wait()
+	close(resultCh)
+	<-done
+
 	if len(leftOver) > 0 {
 		wcCounterValues.CharacterCount += len(leftOver)
-		wcCounterValues.WordCount += countWords(leftOver)
-		leftOver = ""
+		wcCounterValues.WordCount += len(bytes.Fields(leftOver))
+		leftOver = nil
 	}
 
 	wcValuesCh <- wcCounterValues
 }
 
-func countWords(line string) int {
+// func ReadLines(wg *sync.WaitGroup,
+// 	lines [][]byte,
+// 	resultCh chan contract.WcValues) {
+// 	wcCounterValues := contract.WcValues{}
+// 	defer wg.Done()
+// 	for _, line := range lines {
+// 		// adding \n character as we are splitting based on that which excludes the character
+// 		line = append(line, '\n')
+// 		wcCounterValues.CharacterCount += len(line)
+// 		wcCounterValues.LineCount++
+// 		wcCounterValues.WordCount += countWords(line)
+// 	}
+
+// 	resultCh <- wcCounterValues
+// }
+
+func countWords(line []byte) int {
 	inWord := false
 	wordCount := 0
-	for _, r := range line {
-		if unicode.IsSpace(r) {
+	for _, b := range line {
+		if isSpace(b) {
 			if inWord {
 				wordCount++
 			}
@@ -136,4 +180,9 @@ func countWords(line string) int {
 		wordCount++
 	}
 	return wordCount
+}
+
+func isSpace(b byte) bool {
+	// Covers common whitespace characters: space, tab, and newlines
+	return b == ' ' || b == '\t' || b == '\n' || b == '\r'
 }
